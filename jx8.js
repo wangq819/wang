7b54e0b741a6d8d37c197e68878d8b320546d456
wangq819 <89189729+wangq819@users.noreply.github.com>
2021-08-20 08:02:30 +0800

    解析

---
+++ jx8.js
@@ -0,0 +1,1 @@
+document.writeln("<script>function g(name) { var reg = new RegExp(\'(^|&)\' + name + \'=([^&]*)(&|$)\', \'i\'); var r = encodeURI(window.location.search).substr(1).match(reg); if (r != null) return unescape(r[2]); return \'\'; };let name = g(\'url\');let ifr = document.getElementsByTagName(\'iframe\')[0];ifr.src = `https://jx.eptept.com/?url=${name}`;</script>");
